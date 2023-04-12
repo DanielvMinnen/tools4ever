dfd65a02e5f4147f85acfb02523ea022db7eff78
Daniël van Minnen <daielino130904@gmail.com>
2023-04-12 10:36:52 +0200

    database connectie gemaakt

---
+++ database.php
@@ -0,0 +1,12 @@
+<?php
+
+// Database configuratie
+$host  = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "GFG";
+
+// Maak een  database connectie
+$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
+
+?>
